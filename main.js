c1cf3c3d0d010aaa54f998b5d0dfae8776bb0c4e
Denis Kisina <kisinad@gmail.com>
2019-06-19 10:05:05 +0300

    changed the for loop

--- main.js
+++ main.js
@@ -4,13 +4,12 @@ var primeNumber = (endNumber) =>{
 	//let i=0;
 	//while(i<endNumber)yield i++
 
-	for (var i=0;i<arrayOfNumbers.length;i++)
-	{ //loops through array
-    	if(arrayOfNumbers[i]<2 || (arrayOfNumbers[i]%arrayOfNumbers[i]+2)===0 )
+	for (var i=0;i<arrayOfNumbers.length;i++){ //loops through array
+    	if(arrayOfNumbers[i]<2 || (arrayOfNumbers[i]%arrayOfNumbers[i]+1)===0 )
 			    return false;
-    		for(var i=arrayOfNumbers[i]; i<arrayOfNumbers[arrayOfNumbers.length-1]; i++){
+    	for(var i=arrayOfNumbers[i]; i<arrayOfNumbers[arrayOfNumbers.length-1]; i++){
 			//selectedPrimeNumbers[]=i;
-			}
+			
 		selectedPrimeNumbers.push(arrayOfNumbers[i]); //pushes even numbers to the array called selectedPrimeNumbers
 			//console.log(arrayOfNumbers.length);
 		}
